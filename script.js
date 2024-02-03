39722786f5dcadfd08b6be12f3b92df0d326d272
Munmun Das <munmundas@Munmuns-MacBook-Air.local>
2024-02-03 15:39:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log ("Hi, This is Munmun!");
+fullName = "Tony Stark";
+console.log("fullName");
+
+//This is a comment.
+console.log ("Hi, This is Munmun!");
+fullName = "Tony Stark";
+console.log("fullName");
+
+let num = prompt("Enter a number:");
+
+if (num % 5 === 0) {
+    console.log(num,"is a multiple of 5.");
+} else {
+    console.log(num,"is not a multiple of 5.");
+}
+
+let marks = prompt("Enter your marks:");
+
+if (marks <= 100 && marks >= 80) {
+    console.log("Grade A");
+
+} else if 
+    (marks <= 89 && marks >= 70) {
+        console.log("Grade B");
+
+}else if 
+    (marks <= 69 && marks >= 60) {
+        console.log("Grade C");
+    } else if 
+        (marks <= 59 && marks >= 50) {
+            console.log("Grade D");
+
+    } else {
+        console.log("Grade F");
+
+    }
+    
